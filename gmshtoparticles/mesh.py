d61583d3adfba78539e9c93638505b66041bebcc
Patrick Diehl <me@diehlpk.de>
2023-11-01 21:51:11 -0500

    Format mesh

--- gmshtoparticles/mesh.py
+++ gmshtoparticles/mesh.py
@@ -8,68 +8,76 @@ import numpy as np
 import os
 import vtk
 
-class GmshToParticles():
-    
-    def __init__(self, element_type = 'triangle', mesh_input = 'input.msh', output = 'output', print_id = True, norm=0, angle=0):
+
+class GmshToParticles:
+    def __init__(
+        self,
+        element_type="triangle",
+        mesh_input="input.msh",
+        output="output",
+        print_id=True,
+        norm=0,
+        angle=0,
+    ):
         self.angle = angle
         mesh = meshio.read(mesh_input)
         self.points = mesh.points
         self.cells = mesh.cells
-        print("Successfully opened input mesh file" )       
-        
+        print("Successfully opened input mesh file")
+
         if norm == 1:
             self.normalize()
-        
+
         vtu_output = output + "_grid.vtu"
-        #meshio.write(vtu_output, self.points, self.cells, cell_data={"index": [np.arange(len(self.cells))}])
-        print(".vtu output file written at", vtu_output)        
-        
+        # meshio.write(vtu_output, self.points, self.cells, cell_data={"index": [np.arange(len(self.cells))}])
+        print(".vtu output file written at", vtu_output)
+
         if element_type == "triangle":
             csv_output = output + ".csv"
             self.textFile_triangle(csv_output, print_id)
-            print(".csv output file written at", csv_output)  
+            print(".csv output file written at", csv_output)
         elif element_type == "quad":
             csv_output = output + ".csv"
             self.textFile_square(csv_output, print_id)
-            print (".csv output file written at", csv_output)  
+            print(".csv output file written at", csv_output)
         else:
-            print ("Supported element_type are triangle or quad")
-         
-            
+            print("Supported element_type are triangle or quad")
+
         vtk_output = output + ".vtu"
         self.vtkFile(vtk_output, element_type)
-        print (".vtu output file written at", vtk_output)
+        print(".vtu output file written at", vtk_output)
 
     def centerTriangle(self, node):
         x = self.points[node[0]]
         y = self.points[node[1]]
         z = self.points[node[2]]
-        return np.array((x[0] + y[0] + z[0]  , x[1] + y[1] + z[1])) * (1./3.)
-        
-        
+        return np.array((x[0] + y[0] + z[0], x[1] + y[1] + z[1])) * (1.0 / 3.0)
+
     def centerSquare(self, node):
         a = self.points[node[0]]
         b = self.points[node[1]]
         c = self.points[node[2]]
         d = self.points[node[3]]
-        return np.array((a+b+c+d)/4.)
-    
+        return np.array((a + b + c + d) / 4.0)
+
     def areaTriangle(self, node):
         a = self.points[node[1]] - self.points[node[0]]
         b = self.points[node[2]] - self.points[node[0]]
-        return np.linalg.norm(np.cross(a[:] , b[:])) / 2.
-        
+        return np.linalg.norm(np.cross(a[:], b[:])) / 2.0
+
     def areaSquare(self, node):
         a = self.points[node[1]] - self.points[node[0]]
         b = self.points[node[2]] - self.points[node[0]]
-        return np.linalg.norm(a*b)
-   
-    def rotate(self,node):
+        return np.linalg.norm(a * b)
+
+    def rotate(self, node):
         rad = self.angle * np.pi / 180
-        rotatingMatrix = np.array([[np.cos(rad),-np.sin(rad)],[np.sin(rad),np.cos(rad)]])
+        rotatingMatrix = np.array(
+            [[np.cos(rad), -np.sin(rad)], [np.sin(rad), np.cos(rad)]]
+        )
         return np.array(rotatingMatrix.dot(np.array(node)))
 
-    #For now renamed function textfile_triangle
+    # For now renamed function textfile_triangle
     def textFile_triangle(self, outFile, print_id):
         with open(outFile, "w") as file:
             if print_id:
@@ -83,16 +91,20 @@ class GmshToParticles():
                         area = self.areaTriangle(node)
                         center = self.centerTriangle(node)
                         center = self.rotate(center)
-                        
+
                         if print_id:
-                            line = "{:d}, {:.2e}, {:.2e}, {:.2e}".format(i ,center[0], center[1], area )
+                            line = "{:d}, {:.2e}, {:.2e}, {:.2e}".format(
+                                i, center[0], center[1], area
+                            )
                         else:
-                            line = "{:.2e}, {:.2e}, {:.2e}".format(center[0], center[1], area )
+                            line = "{:.2e}, {:.2e}, {:.2e}".format(
+                                center[0], center[1], area
+                            )
                         file.write(line + os.linesep)
                         i += 1
             file.close()
-            
-    #For now renamed function textfile_square
+
+    # For now renamed function textfile_square
     def textFile_square(self, outFile, print_id):
         with open(outFile, "w") as file:
             if print_id:
@@ -106,23 +118,27 @@ class GmshToParticles():
                         area = self.areaSquare(node)
                         center = self.centerSquare(node)
                         center = self.rotate(center)
-                        
+
                         if print_id:
-                            line = "{:d}, {:.2e}, {:.2e}, {:.2e}".format(i ,center[0], center[1], area )
+                            line = "{:d}, {:.2e}, {:.2e}, {:.2e}".format(
+                                i, center[0], center[1], area
+                            )
                         else:
-                            line = "{:.2e}, {:.2e}, {:.2e}".format(center[0], center[1], area )
+                            line = "{:.2e}, {:.2e}, {:.2e}".format(
+                                center[0], center[1], area
+                            )
                         file.write(line + os.linesep)
                         i += 1
             file.close()
-    
-    #needs to be rewritten for square and triangle
-    def vtkFile(self, outFile, type_ ):
+
+    # needs to be rewritten for square and triangle
+    def vtkFile(self, outFile, type_):
         number = 0
         nodes = []
         for cell in self.cells:
             if cell.type == type_:
                 number = len(cell.data)
-                nodes = cell.data 
+                nodes = cell.data
 
         writer = vtk.vtkXMLUnstructuredGridWriter()
         writer.SetFileName(outFile)
@@ -131,45 +147,40 @@ class GmshToParticles():
         points.SetNumberOfPoints(number)
         points.SetDataTypeToDouble()
         dataOut = grid.GetPointData()
-       
+
         array = vtk.vtkDoubleArray()
         array.SetName("Volume")
         array.SetNumberOfComponents(1)
         array.SetNumberOfTuples(number)
-       
+
         if type_ == "triangle":
-            for i in range (0,len(nodes)):
+            for i in range(0, len(nodes)):
                 center = self.centerTriangle(nodes[i])
                 center = self.rotate(center)
                 area = self.areaTriangle(nodes[i])
-                points.InsertPoint(i,center[0],center[1],0)
-                array.SetTuple1(i,area)
+                points.InsertPoint(i, center[0], center[1], 0)
+                array.SetTuple1(i, area)
         elif type_ == "quad":
-            for i in range (0,len(nodes)):
+            for i in range(0, len(nodes)):
                 center = self.centerSquare(nodes[i])
                 center = self.rotate(center)
                 area = self.areaSquare(nodes[i])
-                points.InsertPoint(i,center[0],center[1],0)
-                array.SetTuple1(i,area)
+                points.InsertPoint(i, center[0], center[1], 0)
+                array.SetTuple1(i, area)
 
-       
         grid.SetPoints(points)
         dataOut.AddArray(array)
-       
+
         writer.SetInputData(grid)
-    
+
         writer.GetCompressor().SetCompressionLevel(0)
         writer.SetDataModeToAscii()
         writer.Write()
 
     def normalize(self):
-        
-        minDirection = np.zeros((3))    
-        minDirection[0] = min(self.points[:,0])
-        minDirection[1] = min(self.points[:,1])
-        minDirection[2] = min(self.points[:,2])
-        
+        minDirection = np.zeros((3))
+        minDirection[0] = min(self.points[:, 0])
+        minDirection[1] = min(self.points[:, 1])
+        minDirection[2] = min(self.points[:, 2])
+
         self.points -= minDirection
-        
-        
-        

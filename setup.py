0adfb2d688eb033138555cf568a8af786509e0ba
Patrick Diehl <me@diehlpk.de>
2023-11-06 20:39:07 -0600

    Format

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from setuptools import setup
 setup(
     name="gmshtoparticles",
     version="0.1",
-    description=("Transforms a .msh file generated with Gmsh into a particles centered" 
-    "inside each triangle or quad element. Outputs a .csv file description" 
-    "and .vtk/.vtu visualization set of files"),
+    description=(
+        "Transforms a .msh file generated with Gmsh into a particles centered"
+        "inside each triangle or quad element. Outputs a .csv file description"
+        "and .vtk/.vtu visualization set of files"
+    ),
     url="https://github.com/IaPCS/gmsh-to-nodes/",
     author="Patrick Diehl, Ilyass Tabiai",
     author_email="me@diehlpk.de, ilyass.tabiai@gmail.com",
